7276a79a6075321c70cb293fb239af5493210eea
Shin-sibainu <shincode0712@gmail.com>
2024-06-20 12:25:11 +0900

    meta

---
+++ src/lib/constants.js
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: "ShinCode",
+  siteDesc: "ShinCodeの技術ブログサイト",
+  siteUrl: "https://twitter.com",
+  siteType: "website",
+  siteLocale: "ja_JP",
+  siteIcon: "/favicon.png",
+  siteImg: "/ogp.jpg",
+};
